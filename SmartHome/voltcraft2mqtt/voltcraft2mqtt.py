f75828dcd9d70dafc3f58e058615e77c547a29e4
Horst Fleischer <fleishor@gmail.com>
2025-01-12 11:21:07 +0100

    voltcraft2mqtt comments added

--- SmartHome/voltcraft2mqtt/voltcraft2mqtt.py
+++ SmartHome/voltcraft2mqtt/voltcraft2mqtt.py
@@ -6,12 +6,18 @@ import sys
 import threading
 import logging
 
+# Import SEM6000 module
 from sem6000 import sem6000 as sem6000Module
 from sem6000.message import *
 from sem6000 import util
+
+# Import MQTT module
 from paho.mqtt import client as mqttClientModule
+
+# Import JSON formatter for logging
 from pythonjsonlogger.jsonlogger import JsonFormatter
 
+# Setup logger and set JSON as output format
 logger = logging.getLogger(__name__)
 logHandler = logging.StreamHandler()
 formatter = JsonFormatter(
@@ -20,14 +26,19 @@ formatter = JsonFormatter(
        rename_fields={"levelname": "level"})
 logHandler.setFormatter(formatter)
 logger.addHandler(logHandler)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 
+# MQTT settings
 CLIENT_ID = "voltcraft2mqtt"
 BROKER = "docker.fritz.box"
 PORT = 1883
+
+#Lock for thread safe oparations on SEM6000 device
 Sem6000Lock = threading.Lock()
 
+# Function to connect to MQTT broker
 def connectToMqtt():
+       # Callback function for MQTT connection
        def on_connect(client, userdata, flags, rc, properties):
               if rc == 0:
                      logger.info("Connected to MQTT Broker!")
@@ -44,18 +55,22 @@ def connectToMqtt():
 
        return mqttClient
 
+# Publish device availability status to HomeAssistant
 def setDeviceInHaAsAvailable(mqttClient: mqttClientModule):
        logger.info("Set device in HomeAssistant as available")
        result = mqttClient.publish("sem6000/openmediavault/available", "online")
 
+# Publish power state OFF to HomeAssistant
 def setPowerStateInHaToOff(mqttClient: mqttClientModule):
        logger.info("Set power state in HomeAssistant to OFF")
        result = mqttClient.publish("sem6000/openmediavault/switch/state", "OFF")
 
+# Publish power state ON to HomeAssistant
 def setPowerStateInHaToOn(mqttClient: mqttClientModule):
        logger.info("Set power state in HomeAssitant to ON")
        result = mqttClient.publish("sem6000/openmediavault/switch/state", "ON")
       
+# Initialize MQTT connection, set initial states and subscribe to command topic
 def initialize():
        logger.info("Initializing voltcraft2mqtt")
        mqttClient = connectToMqtt()
@@ -66,6 +81,7 @@ def initialize():
        
        return mqttClient
 
+# Send SEM6000 sensor values to HomeAssistant
 def sendSem6000SensorValuesToHa(mqttClient: mqttClientModule):
        logger.info("Send SEM6000 sensor values to HomeAssistant")
        deviceAddr = sys.argv[1]
@@ -93,6 +109,7 @@ def sendSem6000SensorValuesToHa(mqttClient: mqttClientModule):
        payloadJson = json.dumps(payload)
        result = mqttClient.publish("sem6000/openmediavault/sensor/values", payloadJson)
 
+# Switch power state of SEM6000 device
 def switchPowerState(newSwitchState: str):
        logger.info("Switch power state")
        deviceAddr = sys.argv[1]
@@ -107,6 +124,7 @@ def switchPowerState(newSwitchState: str):
                      sem6000Client.power_off()
               sem6000Client.disconnect()
 
+# Subscribe to MQTT command topic
 def subscribeToCommandTopic(mqttClient: mqttClientModule):
     def on_message(client, userdata, msg):
        switchPowerState(msg.payload.decode())     
@@ -115,6 +133,7 @@ def subscribeToCommandTopic(mqttClient: mqttClientModule):
     mqttClient.subscribe("sem6000/openmediavault/switch/set")
     mqttClient.on_message = on_message
 
+# Main loop to send sensor values periodically
 def run(mqttClient: mqttClientModule):
        logger.info("Start main loop")
        mqttClient.loop_start()
